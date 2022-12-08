502557bf87db460aa92b9e53d1795fe73f8266b2
Annie Renhorn <h21annre@du.se>
2022-12-08 11:29:21 +0100

    Det funkar nog

--- gik2f8-ht22-01-searchlist-p03/script.js
+++ gik2f8-ht22-01-searchlist-p03/script.js
@@ -24,5 +24,47 @@ function renderBookList(bookList) {
   const root = document.getElementById('root');
 
   existingElement && root.removeChild(existingElement);
+  BookList.length > 0 && searchField.value && root.insertAdjacentHTML
+  ('beforeend', BookList(bookList));
+}
+
+ function handleMouseover(li){ 
+}
+
+
+function renderBookInformation(FoundBookId, x, y){
+  let bookInfo = `<div ID="bookInfo" class="absolute w-80 h-100 gap-4 
+  p-10  bg-pink-300 border-black-10 m-10 top-[${
+    y + 10
+  }px] left-[${x + 10}px]">
+      <h2> Author ${FoundBookId.author}</h2>    
+      <p class="m-2 text-black font-semibold"> Title: ${FoundBookId.title}</p>
+      <p class="m-2 text-black font-semibold"> Pages: ${FoundBookId.pages}</p>
+      <p class="m-2 text-black font-semibold"> Release date: ${FoundBookId.releaseDate}</p>
+      <img src="${FoundBookId.coverImage}" alt="Image is missing" width="200px" height="220px">
+        
+</div>`
+      root.insertAdjacentHTML('beforebegin', bookInfo);
+}
+
+
+
+function renderBookList(bookList) {
+  const existingElement = document.querySelector('.book-list');
+
+  const root = document.getElementById('root');
+
+  existingElement && root.removeChild(existingElement);
   bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
+  for (let bookId of document.getElementsByClassName("book-list__item")){
+    bookId.addEventListener("mouseenter", (e) => {
+      
+    getOne(bookId.getAttribute("ID"))
+      
+    })
+    bookId.addEventListener("mouseleave", (e) => {
+      bookInfo && bookInfo.remove();
+      
+    })
+  }
 }
